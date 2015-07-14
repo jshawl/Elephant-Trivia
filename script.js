38c2d68670f6fc323fefa79ea15f7b0bebbfc334
Rachel Warren <raelwa@gmail.com>
2015-07-14 09:51:05 -0400

    added splice to stop repeat questions

--- script.js
+++ script.js
@@ -11,44 +11,45 @@ function getRandomQuestion() {
   triviaQuestions.eq(random).show();
   $(".otherAnswer").css("background-color", "white");
   $(".correctAnswer").css("background-color", "white");
-
+  triviaQuestions.splice(random, 1);
 }
-  // alert("jquery works!")
 
 
 $("img").eq(0).on("click", function(){
 
-    scoreCount = 0;
-    questionNumber = 1;
-    $(".scoreCard").html("Score: " + scoreCount);
-    $(".info").html("Question " + questionNumber + " out of " +  triviaQuestions.length + ":");
+  scoreCount = 0;
+  questionNumber = 1;
+  $(".scoreCard").html("Score: " + scoreCount);
+  $(".info").html("Question " + questionNumber + " out of " +  triviaQuestions.length + ":");
 
     // console.log("about to call func");
 
+  getRandomQuestion();
+
+
+  $(".correctAnswer").on("click", function(){
+    $(".correctAnswer").css("background-color", "#02CC6B");
+    questionNumber++;
+    scoreCount++;
+    $(".scoreCard").html("Score: " + scoreCount);
+    $(".question").css("display", "none");
+    $(".info").html("Question " + questionNumber + " out of " +  $(".question").length + ":");
     getRandomQuestion();
+  });
 
+  $(".otherAnswer").on("click", function(){
+    $(this).css("background-color", "#F04A5A");
+    $(".correctAnswer").css("background-color", "#02CC6B");
+    scoreCount += 0;
+    questionNumber++;
+    $(".question").css("display", "none");
+    $(".info").html("Question " + questionNumber + " out of " +  $(".question").length + ":");
+    getRandomQuestion();
+  });
+  //
+  // if (questionNumber > triviaQuestions.length) {
+  //   $(".info").html("Game over!");
+  // }
 
-      $(".correctAnswer").on("click", function(){
-        $(".correctAnswer").css("background-color", "#02CC6B");
-        questionNumber++;
-        scoreCount++;
-        $(".scoreCard").html("Score: " + scoreCount);
-        $(".question").css("display", "none");
-        $(".info").html("Question " + questionNumber + " out of " +  $(".question").length + ":");
-        getRandomQuestion();
-      });
-
-      $(".otherAnswer").on("click", function(){
-        $(this).css("background-color", "#F04A5A");
-        $(".correctAnswer").css("background-color", "#02CC6B");
-        scoreCount += 0;
-        questionNumber++;
-        $(".question").css("display", "none");
-        $(".info").html("Question " + questionNumber + " out of " +  $(".question").length + ":");
-        getRandomQuestion();
-      });
-
-
-
-}) //documet.ready closing tag
+})
 })
