b2df048c371481e4c11a9aba15d63b1c09602d89
Jesse Shawl <jesse@jshawl.com>
2015-07-19 16:48:53 -0400

    add feedback

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var scoreCount;
 var questionNumber;
+// any reason these are outside the doc.ready below?
 
 $(document).ready(function() {
 
 var triviaQuestions = $(".question");
+// some people prefer to name jQuery variables with a dollar sign.
+// e.g. $triviaQuestions
 
   function getRandomQuestion() {
     var random = Math.floor(Math.random() * triviaQuestions.length);
     console.log(random);
     triviaQuestions.eq(random).show();
-    $(".otherAnswer").css("background-color", "white");
+    $(".otherAnswer").css("background-color", "white"); // or toggle a class and manage styles in CSS
     $(".correctAnswer").css("background-color", "white");
     triviaQuestions.splice(random, 1);
   }
@@ -29,12 +32,15 @@ var triviaQuestions = $(".question");
       $(".correctAnswer").css("background-color", "#02CC6B");
       questionNumber++;
       scoreCount++;
-      $(".scoreCard").html("Score: " + scoreCount).css("color", "red");
+      $(".scoreCard").html("Score: " + scoreCount).css("color", "red"); // the color red made me think I got it wrong
       $(".next").show();
     });
 
     $(".otherAnswer").on("click", function(){
       $(this).css("background-color", "#F04A5A");
+      // I recommend toggling a class instead of manipualting inline styles here
+      // this will allow you to separate your concerns by keeping styles in CSS files
+      // and behavior in JS files.
       $(".correctAnswer").css("background-color", "#02CC6B");
       questionNumber++;
       scoreCount += 0;
@@ -42,7 +48,7 @@ var triviaQuestions = $(".question");
     });
 
     $(".next").on("click", function(){
-      $(".question").css("display", "none");
+      $(".question").css("display", "none"); // or .hide()
       $(".info").html("Question " + questionNumber + " out of " +  $(".question").length + ":");
       $(".next").hide();
       getRandomQuestion();
@@ -60,3 +66,11 @@ var triviaQuestions = $(".question");
   })
 
 })
+
+// Overall:
+// Excellent solution! I love that you didn't have to hardcode individual questions and
+// click handlers. This means you could very easily add more questions if you choose to do
+// so in the future.
+//
+// I recommend creating more variables for each of the jQuery selectors you have to increase
+// performance, and querying the DOM is an expensive task.
